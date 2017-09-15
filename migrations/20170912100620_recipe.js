1911fd9f331d79481ca261187fe4fd2a636eb6b8
KevinMAnderson <kma2k4@hotmail.com>
2017-09-15 10:40:53 -0600

    revised ERD to make ingredient_quanity_step in lieu of ingredient_quanity_recipe

--- migrations/20170912100620_recipe.js
+++ migrations/20170912100620_recipe.js
@@ -3,9 +3,17 @@ exports.up = function(knex, Promise) {
     return knex.schema.createTable('recipe', (table)=>{
         table.increments();
         table.text('name');
-        table.text("person_name")
+        table.text('person_name')
       .references("person.name")
       .onDelete("CASCADE");
+      table.text('description');
+      table.text('image');
+      table.integer('ingredient_id')
+      .references('ingredient.id')
+      .onDelete('CASCADE');
+      table.integer('step_id')
+      .references('step.id')
+      .onDelete('CASCADE');
 
     });
 };
--- seeds/04recipe.js
+++ seeds/04recipe.js
@@ -7,7 +7,21 @@ exports.seed = function(knex, Promise) {
       return knex('recipe').insert([
         {id: 1,
         name: 'Kev Pepperoni Pizza',
-        person_name : "Kevin Anderson",
+        person_name : 'Kevin Anderson',
+        description: 'The best pizza ever',
+        image: 'pizza.jpeg',
+        ingredient_id: 1,
+        ingredient_id: 2,
+        ingredient_id: 3,
+        ingredient_id: 4,
+        ingredient_id: 5,
+        step_id: 1,
+        step_id: 2,
+        step_id: 3,
+        step_id: 4,
+        step_id: 5,
+        step_id: 6,
+        step_id: 7,
     },
 
 ]);
