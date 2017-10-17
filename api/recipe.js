1494e6166d541f4f15ff6c31764c9327cdd4b154
KevinMAnderson <kma2k4@hotmail.com>
2017-10-16 20:15:38 -0600

    finished get all route...post recipe route still not working

--- api/recipe.js
+++ api/recipe.js
@@ -3,14 +3,111 @@ const express = require( 'express' );
 const router = express.Router();
 const queries = require( '../db/recipes_queries' );
 
+// validate ID
+function isValidId(request, response, next) {
+    if (!isNaN(request.params.id)) return next();
+    next(new Error('Invalid ID'));
+}
+// validate Recipe
+function validRecipe(recipe) {
+  // IS THE TITLE A STRING? AND DOES IT HAVE A VALUE INSIDE?
+  const hasTitle = typeof recipe.title == "string" && recipe.title.trim() != "";
 
-//Route : get all recipes
-router.get('/', (request, response)=>{
-    
+  const hasRecipeDescription = typeof recipe.recipeDescription == "string" && recipe.recipeDescription.trim() != "";
+
+  const hasUrl = typeof recipe.file == "string" && recipe.file.trim() != "";
+
+  const hasName = typeof recipe.personName == "string" && recipe.personName.trim() != "";
+
+  const hasingredientName = typeof recipe.ingredient[0].ingredientName == "string" && recipe.ingredient[0].ingredientName.trim() != "";
+
+  const hasingredientUnit = typeof recipe.ingredient[0].unit == "string" && recipe.ingredient[0].unit.trim() != "";
+
+   const hasingredientAmount = typeof recipe.ingredient[0].amount == "string" && recipe.ingredient[0].amount.trim() != "";
+
+   const hasStep = typeof recipe.step[0].order == "string" && recipe.step.order.trim() != "";
+
+   const hasstepDescription = typeof recipe.step[0].stepDescription == "string" && recipe.step[0].stepDescription.trim() != "";
+
+ // RETURN THE VALUES OF EACH VARIABLE
+  return hasTitle && hasRecipeDescription && hasUrl && hasName && hasingredientName && hasingredientUnit && hasingredientAmount && hasStep && hasstepDescription;
+}
+
+function read (request, response){
     queries.getAllRecipes().then( recipe =>{
         response.json(recipe);
     });
-});
+}
+
+function readId (request,response){
+    queries.getRecipeById(request.params.id).then( recipe =>{
+        if(recipe) {
+            response.json(recipe);
+        }
+        else {
+            next();
+        }
+    })
+}
+
+function post( request, response, next ) {
+    
+    queries.createRecipe(request.body).then(()=>{
+
+        response.json( 'Recipe Added!' );
+    });
+        // if(validRecipe(request.body)) {
+        //
+        //     queries.createRecipe(request.body).then(()=>{
+        //
+        //         response.json( 'Recipe Added!' );
+        //     });
+        // }
+        //
+        // else {
+        //     next(new Error ('Invalid Recipe'));
+        // }
+
+}
+
+function remove (request, response, next) {
+    queries.removeRecipe(request.params.id)
+            .then(()=>{
+                response.json('Recipe Deleted');
+            });
+        }
+
+function put (request, response, next) {
+    if(validRecipe(request.body)){
+
+        queries.updateRecipe(request.params.id, request.body)
+                        .then(()=>{
+                            response.json('Recipe Updated')
+                        });
+        }else {
+            next( new Error('Invalid Recipe Update'));
+    }
+}
+
+
+
+
+//Route : get all recipes
+router.get('/', read);
+
+// Route : get recipe by id
+router.get('/:id', isValidId, readId);
+
+//Route : add recipe
+router.post('/', post);
+
+
+// Update RECIPE
+router.put('/:id', put );
+
+// DELETE A RECIPE
+router.delete('/:id', remove );
+
 
 
 module.exports = router;
--- db/recipes_queries.js
+++ db/recipes_queries.js
@@ -4,8 +4,170 @@ const knex = require('./knex'); //the connection
 
 module.exports = {
 
+
+
     getAllRecipes(){
-        return knex('recipe');
+
+        function reformattedRecipe (recipes){
+
+            const reformatted = [];
+            //a place to look up recipe
+            const recipeById = {};
+
+            recipes.forEach(recipe=>{
+
+                if (recipeById[recipe.id]  ){
+
+                    function checkIfOrder  (array, item){
+                        return array.some(n => n.order === item );
+                    }
+
+                    function checkIfIng(array, item){
+                        return array.some(n => n.ingredientName === item );
+                    }
+
+
+                        if (!checkIfOrder(recipeById[recipe.id].step,recipe.order) ) {
+                            recipeById[recipe.id].step.push(
+                                {
+                                order : recipe.order,
+                                stepDescription : recipe.stepDescription,
+                                }
+                            );
+                        }
+
+
+
+                        if (!checkIfIng(recipeById[recipe.id].ingredient,recipe.ingredientName) ) {
+                            // console.log(ingredient.ingredientName);
+                            recipeById[recipe.id].ingredient.push(
+                                {
+                                    ingredientName :recipe.ingredientName,
+                                    amount: recipe.amount,
+                                    unit: recipe.unit,
+                                }
+                            );
+                        }
+
+                }
+
+            // if recipes does not exist in reformatted
+            else {
+                    recipeById[recipe.id] = {
+                    id : recipe.id,
+                    title : recipe.title,
+                    recipeDescription : recipe.recipeDescription,
+                    file: recipe.file,
+                    person_personName: recipe.person_personName,
+                    step : [{
+                        order : recipe.order,
+                        stepDescription : recipe.stepDescription,
+                    }],
+                    ingredient:[ {
+                        ingredientName :recipe.ingredientName,
+                        amount: recipe.amount,
+                        unit: recipe.unit,
+                    }],
+
+                }
+               reformatted.push(recipeById[recipe.id]);
+            }
+
+
+            });
+            return reformatted;
+        }
+
+        // get all recipe
+        return knex('recipe').select('recipe.id',
+        'title',
+        'recipeDescription',
+        'file',
+        'person_personName',
+        'ingredientName',
+        'amount',
+        'unit',
+        'order',
+        'stepDescription')
+        .join('ingredient as ing','ing.recipe_id', "=",'recipe.id')
+        .join('step as stp','stp.recipe_id', "=", 'recipe.id')
+        .then(data =>{
+        return reformattedRecipe(data);
+        });
+    },
+
+    getRecipeById(id){
+        return knex('recipe').where('id', id).first();
+    },
+    removeRecipe (id) {
+
+        return knex('recipe')
+            .where('id', id)
+            .del();
+    },
+
+    updateRecipe (id, recipe) {
+        return knex('recipe')
+            .where('id', id)
+            //make sure object from front end matches table keys
+            .update(recipe, "*");
+    },
+
+    createRecipe (recipe) {
+
+        /////// HELPER FUNCTIONS //////////////////////
+        const addRecipe = ( data ) => {
+            return knex( 'recipe' )
+                .returning( 'id' )
+                .insert( {
+                    title: data.title,
+                    recipeDescription: data.recipeDescription,
+                    file: data.file,
+                    person_personName: data.personName,
+                } )
+                .then( function ( id ) {
+                        //TODO error invalid input syntax for integer: "{"17"}"....17 is incrementing id
+                    console.log(id)
+
+                    let steps = data.step;
+                    // console.log(data);
+
+                    let stepsPromises = steps.map( ( step ) => {
+
+                        return knex( 'step' ).insert( {
+                            order: step.order,
+                            stepDescription: step.stepDescription,
+                            recipe_id: id,
+                        } );
+                    } );
+                    return Promise.all( stepsPromises );
+                } );
+
+        }
+
+        const addPerson = ( data ) => {
+
+            return knex( 'person' ).insert( {
+                personName: data.personName
+            } );
+        };
+
+        const addIngredient = ( data ) => {
+            let ingredients = data.ingredient;
+            let ingredientsPromises = ingredients.map( ( ingredient ) => {
+                return knex( 'ingredient' ).insert( {
+                    ingredientName: ingredient.ingredientName,
+                    unit: data.unit,
+                    amount: data.amount,
+                } );
+            } );
+            return Promise.all( ingredientsPromises );
+        }
+        addRecipe( recipe );
+        addPerson( recipe );
+        addIngredient( recipe );
     }
 
-};
+
+
+}
